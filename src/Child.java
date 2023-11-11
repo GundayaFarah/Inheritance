c499ef06a01a4ca3b29f61a279baa8cc699f77e5
GundayaFarah <farahgundaya123@gmail.com>
2023-11-11 08:31:40 +0800

    first file

---
+++ src/Child.java
@@ -0,0 +1,15 @@
+
+public class Child extends Parent{
+    
+    Child(String name) {
+        super(name);
+    }
+     void gatanga(){
+        System.out.println(name + " basa permi inig uli kay sig ulan");
+    }
+    
+    void sigegkaon(){
+        System.out.println(name + " sig laag bisag daghan activities");
+    }
+    
+}
---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    
+    public static void main(String[] args){
+        
+        Child myChild = new Child ("Farah Gundaya");
+        
+        myChild.sigbasabasa();
+        myChild.siglaag();
+       
+    }
+}
